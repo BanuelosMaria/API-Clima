0b7f6014f65def2807400a9d7784bff63be75dd7
Maria Banuelos <maria.banuelos193@tectijuana.edu.mx>
2023-02-13 23:11:05 -0800

    comente el codigo incompleto

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ function Leer()
     const pagina_api = 'http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${llave}&units=metric'
     buscar(pagina_api);
 
-    function buscar(pagina_api)
-    {
-        fetch(pagina_api)
-      .then(data => {
-        return data.json()
-      }).then(resultado=>{
-            console.log(resultado);
+    //function buscar(pagina_api)
+    //{
+      //  fetch(pagina_api)
+     // .then(data => {
+     //   return data.json()
+    //  }).then(resultado=>{
+    //        console.log(resultado);
 
-            const {Search=[]} = resultado;
+    //        const {Search=[]} = resultado;
             
-            console.log(Search);
-            document.getElementById("lista").innerHTML='';
+    //        console.log(Search);
+      //      document.getElementById("lista").innerHTML='';
 
-            Search.map((p)=>{
-                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
-                         <img width='100%' src=${p.Poster} alt="No hay poster"></img>
-            </div>`;
-            })
-      });
-    }
+       //     Search.map((p)=>{
+         //       document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+          //               <img width='100%' src=${p.Poster} alt="No hay poster"></img>
+           // </div>`;
+          //  })
+    //  });
+   // }
 }
